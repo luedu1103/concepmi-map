152cb4ecee158efa5fa5dbf8671001738c2d1801
Luis Calle <luedu1103@gmail.com>
2024-09-17 17:16:54 -0500

    hopes this works

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Map, NavigationControl } from 'mapbox-gl/dist/mapbox-gl.js';
+import { Map, NavigationControl, MapboxDirections} from 'mapbox-gl/dist/mapbox-gl.js';
 import mapboxgl from 'mapbox-gl';
 
 const API_KEY = import.meta.env.VITE_API_KEY;
@@ -17,6 +17,7 @@ const map = new Map({
     pitch: 0,
 });
 
+// Zoom, direccion, etc
 map.addControl(new NavigationControl());
 
 map.on('load', () => {
@@ -92,7 +93,6 @@ map.on('load', () => {
     .setLngLat([-79.039117, -8.114850])
     .addTo(map)
     .getElement().addEventListener('click', () => {
-        showRouteToMarker([-79.039117, -8.114850]);
         map.flyTo({
             center: [-79.039117, -8.114850],
             zoom: 15,
@@ -121,7 +121,6 @@ map.on('load', () => {
             .setLngLat(location.coordinates.reverse())
             .addTo(map)
             .getElement().addEventListener('click', () => {
-                showRouteToMarker(location.coordinates);
                 map.flyTo({
                     center: location.coordinates,
                     zoom: 15,
@@ -152,7 +151,6 @@ map.on('load', () => {
             .setLngLat(location.coordinates.reverse())
             .addTo(map)
             .getElement().addEventListener('click', () => {
-                showRouteToMarker(location.coordinates);
                 map.flyTo({
                     center: location.coordinates,
                     zoom: 15,
@@ -201,39 +199,61 @@ closePanelBtn.addEventListener('click', () => {
 });
 
 // Posicion del usuario
-function getUserLocation() {
-    return new Promise((resolve, reject) => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    resolve([position.coords.longitude, position.coords.latitude]);
-                },
-                (error) => {
-                    reject(error);
-                }
-            );
-        } else {
-            reject(new Error("Geolocation is not supported by this browser."));
-        }
-    });
-}
+map.addControl(
+    new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true
+    })
+);
 
-// Direcciones
-map.addControl(new NavigationControl());
-
-// Add Directions Control
-const directions = new MapboxDirections({
-    accessToken: mapboxgl.accessToken,
-    unit: 'metric'
-});
-map.addControl(directions, 'top-left');
-
-async function showRouteToMarker(markerLngLat) {
-    try {
-        const userLocation = await getUserLocation();
-        directions.setOrigin(userLocation);
-        directions.setDestination(markerLngLat);
-    } catch (error) {
-        console.error("Error getting user location:", error);
+// Ruta
+async function getRoute(end) {
+    // make a directions request using cycling profile
+    // an arbitrary start will always be the same
+    // only the end or destination will change
+    const query = await fetch(
+      `https://api.mapbox.com/directions/v5/mapbox/cycling/${start[0]},${start[1]};${end[0]},${end[1]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`,
+      { method: 'GET' }
+    );
+    const json = await query.json();
+    const data = json.routes[0];
+    const route = data.geometry.coordinates;
+    const geojson = {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: 'LineString',
+        coordinates: route
+      }
+    };
+    // if the route already exists on the map, we'll reset it using setData
+    if (map.getSource('route')) {
+      map.getSource('route').setData(geojson);
     }
-}
+    // otherwise, we'll make a new request
+    else {
+      map.addLayer({
+        id: 'route',
+        type: 'line',
+        source: {
+          type: 'geojson',
+          data: geojson
+        },
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#3887be',
+          'line-width': 5,
+          'line-opacity': 0.75
+        }
+      });
+    }
+    // add turn instructions here at the end
+  }
